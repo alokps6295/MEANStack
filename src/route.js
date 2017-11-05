15763f7df3aae07a7568c33aee36d9a792cb03b7
alok <alok.singh@geminisolutions.in>
2017-11-05 23:22:59 +0530

    files and routes commit

--- src/route.js
+++ src/route.js
@@ -2,15 +2,19 @@ angular.module('News').config([
     '$stateProvider',
     '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
-    
       $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: '/home.html',
-          controller: 'MainCtrl'
-        });
-    
-      $urlRouterProvider.otherwise('home');
-    }]);
-    
-    
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
+  
+    $urlRouterProvider.otherwise('home');
+  }]);
+  
+  
